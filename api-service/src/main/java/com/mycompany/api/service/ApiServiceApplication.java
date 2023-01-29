7a8033e50904e69aa5c8fb31f97ffcaa380d8a44
Harold Alcala <harold.dev@gmail.com>
2023-01-29 19:17:48 +0800

    config service, distributed tracing

--- api-service/src/main/java/com/mycompany/api/service/ApiServiceApplication.java
+++ api-service/src/main/java/com/mycompany/api/service/ApiServiceApplication.java
@@ -1,16 +1,11 @@
 package com.mycompany.api.service;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import org.apache.http.client.HttpClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@CircuitBreaker(name = "myCircuitBreaker", fallbackMethod = "/userServiceFallback")
 public class ApiServiceApplication {
 
 	public static void main(String[] args) {
